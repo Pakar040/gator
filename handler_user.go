7cb2b4c01e50d9c0d66f5f3dd6ebdd8eb714bed0
Alek Kariniemi <alekkariniemi1@gmail.com>
2026-06-25 19:02:58 -0700

    Reset Command

--- handler_user.go
+++ handler_user.go
@@ -53,3 +53,16 @@ func handlerRegister(s *state, cmd command) error {
 	fmt.Printf("User has been created: %v\n", createdUser)
 	return nil
 }
+
+func handlerReset(s *state, cmd command) error {
+	if len(cmd.args) != 0 {
+		return fmt.Errorf("Reset command expects 0 args")
+	}
+
+	if err := s.db.DeleteAll(context.Background()); err != nil {
+		return err
+	}
+
+	fmt.Println("All users have been deleted")
+	return nil
+}
