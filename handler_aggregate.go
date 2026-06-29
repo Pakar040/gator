afb789b51a01ef5e8bfd793757f9185d27c2cec2
Alek Kariniemi <alekkariniemi1@gmail.com>
2026-06-28 18:03:49 -0700

    Aggregate

--- handler_aggregate.go
+++ handler_aggregate.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 	"time"
 
 	"github.com/Pakar040/gator/internal/database"
@@ -10,19 +11,50 @@ import (
 )
 
 func handlerAggregate(s *state, cmd command) error {
-	if len(cmd.args) != 0 {
-		return fmt.Errorf("Reset command expects 0 args")
+	if len(cmd.args) != 1 {
+		return fmt.Errorf("Reset command expects 1 arg: time_between_requests")
 	}
 
-	feedUrl := "https://www.wagslane.dev/index.xml"
-	rssFeed, err := fetchFeed(context.Background(), feedUrl)
+	timeBetweenRequests, err := time.ParseDuration(cmd.args[0])
 	if err != nil {
-		return err
+		return fmt.Errorf("invalid duration: %w", err)
 	}
 
-	fmt.Println(rssFeed)
+	log.Printf("Collecting feeds every %s...", timeBetweenRequests)
 
-	return nil
+	ticker := time.NewTicker(timeBetweenRequests)
+
+	for ; ; <-ticker.C {
+		scrapeFeeds(s)
+	}
+}
+
+func scrapeFeeds(s *state) {
+	feed, err := s.db.GetNextFeedToFetch(context.Background())
+	if err != nil {
+		log.Println("Couldn't get next feeds to fetch", err)
+		return
+	}
+	log.Println("Found a feed to fetch!")
+	scrapeFeed(s.db, feed)
+}
+
+func scrapeFeed(db *database.Queries, feed database.Feed) {
+	_, err := db.MarkFeedFetched(context.Background(), feed.ID)
+	if err != nil {
+		log.Printf("Couldn't mark feed %s fetched: %v", feed.Name, err)
+		return
+	}
+
+	feedData, err := fetchFeed(context.Background(), feed.Url)
+	if err != nil {
+		log.Printf("Couldn't collect feed %s: %v", feed.Name, err)
+		return
+	}
+	for _, item := range feedData.Channel.Item {
+		fmt.Printf("Found post: %s\n", item.Title)
+	}
+	log.Printf("Feed %s collected, %v posts found", feed.Name, len(feedData.Channel.Item))
 }
 
 func handlerAddFeed(s *state, cmd command, user database.User) error {
